fbaf33b96fd021c0392ee252087c1260f8e8fa8e
alber1311 <alber1311@hotmail.es>
2020-12-22 11:49:08 +0100

    Correction

---
+++ app/src/main/java/com/example/languidedemo/ui/student/StudentMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.languidedemo.ui.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.languidedemo.*;
+
+public class StudentMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_main);
+    }
+}
