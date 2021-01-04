1ab1f568307c186392065158d34ba9afd809ca03
Sarmatian123 <sarmata12345@gmail.com>
2021-01-04 11:36:07 +0100

    Added TestActivity class.

---
+++ app/src/main/java/com/example/languide/ui/student/TestActivity.java
@@ -0,0 +1,13 @@
+package com.example.languide.ui.student;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.languide.R;
+
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
