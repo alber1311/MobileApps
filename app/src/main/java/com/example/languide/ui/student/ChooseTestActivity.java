bb6cca3a06466540c97e8d5acb93ad4de395a6b8
alber1311 <alber1311@hotmail.es>
2021-01-05 19:07:28 +0100

    Refactor of design in ChooseTestActivity and ChooseDifficultyActivity

--- app/src/main/java/com/example/languide/ui/student/ChooseTestActivity.java
+++ app/src/main/java/com/example/languide/ui/student/ChooseTestActivity.java
@@ -29,7 +29,7 @@ public class ChooseTestActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.idListView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tests);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.custom_list_item, tests);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
